e1b9df330ff9e51380d782e86703b67f696201ee
Enrique Madrigal <emadriga@gmail.com>
2017-10-16 16:11:39 -0500

    Nuevos

---
+++ app/src/main/java/com/datalabor/soporte/manhattan/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.datalabor.soporte.manhattan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
